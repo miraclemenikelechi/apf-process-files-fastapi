1b75624cdefedf89143ab6216dacfd793ad6ae35
miraclemenikelechi <miracle.emenike7@gmail.com>
2024-04-08 14:22:25 +0100

    init fastapi

---
+++ main.py
@@ -0,0 +1,18 @@
+import starlette.status as status
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+server = FastAPI()
+
+
+@server.get("/")
+async def root():
+    """
+    Redirects incoming GET requests to the API documentation page.
+    """
+
+    # Redirecting to the API documentation page using a 302 status code
+    return RedirectResponse(
+        url="/docs",
+        status_code=status.HTTP_302_FOUND,
+    )
